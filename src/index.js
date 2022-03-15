d0dfece34764fa1a04741fa5dfe03c6e9440c1de
TilburgSurviance <101642623+TilburgSurviance@users.noreply.github.com>
2022-03-15 15:22:00 +0100

    a

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { Client, Intents } = require("discord.js")
+const config = require("./config.json")
 const client = new Client({
     intents: [
         Intents.FLAGS.GUILDS
@@ -12,6 +13,26 @@ client.once("ready", () => {
     } catch (error) {
         console.error(error)
     }
+})
+
+
+client.once("messageCreate", message => {
+
+
+if(message.author.bot) return
+
+var prefix = config.prefix
+
+
+var messagearray = message.content.split(" ")
+
+var command = messagearray[0]
+
+if(command == `${prefix}test`) [
+    message.reply("ja wil je testen mijn maatje")
+]
+
+
 })
 
 client.login(process.env.token)
