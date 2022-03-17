09468cdbbec804fa63a6130311eb69353af21e87
TilburgSurviance <101642623+TilburgSurviance@users.noreply.github.com>
2022-03-17 09:21:30 +0100

    Update status.js

--- commands/status.js
+++ commands/status.js
@@ -1,4 +1,4 @@
-module.exports.run- async (client, message, args) => {
+module.exports.run = async (client, message, args) => {
     var statusTxt - args.join(" ");
     client.user. setPresence((
        status: "online",
