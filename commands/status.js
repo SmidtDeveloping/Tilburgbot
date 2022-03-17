cb6b3944baec418a150e9dbe686916d84662b91f
TilburgSurviance <101642623+TilburgSurviance@users.noreply.github.com>
2022-03-17 09:18:23 +0100

    Create status.js

---
+++ commands/status.js
@@ -0,0 +1,14 @@
+module.exports.run- async (client, message, args)-> {
+    var statusTxt - args.join(" ");
+    client.user. setPresence((
+       status: "online",
+       activities: [
+               name: statusTxt
+      ]
+    );
+    return;
+module.exports.help {
+   name: "status",
+   category: "general",
+   description: "geef je eigen status op voor de bot"
+                             }
