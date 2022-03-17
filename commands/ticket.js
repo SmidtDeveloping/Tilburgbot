09648ed05da6370d55dc2b0716329d95c7b99ff0
TilburgSurviance <101642623+TilburgSurviance@users.noreply.github.com>
2022-03-17 11:38:10 +0100

    a

---
+++ commands/ticket.js
@@ -0,0 +1,18 @@
+const discord = require("discord.js")
+module.exports.run  = async (client, message, args) => {
+   
+const catid = "940229466112798770"
+
+var username = message.author.username
+
+var userdiscriminator = message.author.discriminator
+
+var reden = args.join(" ")
+
+}
+
+module.exports.help = {
+ name: "info",  
+ catogory: "Info",
+ description: "Geeft je informatie"
+}
