8161f3fee8fd842ad9465c5d543f30fb038a62a8
TilburgSurviance <101642623+TilburgSurviance@users.noreply.github.com>
2022-03-18 13:10:29 +0100

    aa

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, Intents } = require("discord.js")
+const { Client, Intents, MessageEmbed } = require("discord.js")
 const { Prefix } = require("./config.json")
 const client = new Client({
     intents: [
@@ -22,8 +22,30 @@ client.on("messageCreate", message => {
     var command = messagearray[0]
 
     if(command == `${Prefix}info`) {
-        return message.channel.send("Test")
-    }
+        const embed = new MessageEmbed()
+    embed.setTitle(`ServerInfo voor ${message.guild.name}`)
+    embed.setColor("RANDOM")
+    embed.setFields(
+        {name: "Naam", value: message.guild.name},
+        {name: "membercount", value: message.guild.memberCount.toString()},
+        {name: "Roles", value: message.guild.roles.cache.size.toString()},
+        {name: "Channels", value: message.guild.channels.cache.size.toString()},
+        {name: "Id", value: message.guildId},
+),
+    embed.setThumbnail(message.guild.iconURL())
+
+const botembed = new MessageEmbed()
+botembed.setTitle(`Botinfo voor ${bot.user.username}`)
+botembed.setColor("RANDOM")
+botembed.setFields(
+    {name: "naam:", value: bot.user.username},
+    {name: "id", value: bot.user.id},
+)
+
+
+
+    message.channel.send({embeds: [embed, botembed]})
+        }
  })
 
 client.login(process.env.token)
