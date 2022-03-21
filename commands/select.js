6ae6db6c42e476ad9c6db690f1d3418273a50df9
TilburgSurviance <101642623+TilburgSurviance@users.noreply.github.com>
2022-03-21 20:19:08 +0100

    a

---
+++ commands/select.js
@@ -0,0 +1,21 @@
+const discord = require("discord.js")
+module.exports.run  = async (client, message, args) => {
+
+const options = [
+    {
+        label: "Test",
+        value: "954078384386883634"
+    }
+]
+
+
+const row = discord.messageActionRow()
+
+}
+
+module.exports.help = {
+ name: "status",  
+ catogory: "Info",
+ description: "Geeft je informatie",
+ aliases: []
+}
