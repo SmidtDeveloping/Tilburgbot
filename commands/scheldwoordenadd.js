25ec6199430701ec349366ba0e39c71b9bf44514
TilburgSurviance <101642623+TilburgSurviance@users.noreply.github.com>
2022-03-21 18:47:31 +0100

    Update scheldwoordenadd.js

--- commands/scheldwoordenadd.js
+++ commands/scheldwoordenadd.js
@@ -1,6 +1,7 @@
 const  discord = require("discord.js")
 const fs = require("fs")
 const Perms = require("permissions")
+const file = require("../data/woorden.json")
 module.exports.run  = async (client, message, args) => {
     const member = message.member
     const channel = message.channel
@@ -11,13 +12,13 @@ module.exports.run  = async (client, message, args) => {
 
     var word = args[1]
 
-    const scheldwoordenJSON = fs.readFileSync("../data/woorden.json", 'utf-8')
+    const scheldwoordenJSON = fs.readFileSync(file, 'utf-8')
     const scheldwoorden = JSON.parse(scheldwoordenJSON)
 
     scheldwoorden.push(word)
 
     scheldwoordenJSON = JSON.stringify(scheldwoorden)
-    fs.writeFileSync("../data/woorden.json", scheldwoordenJSON, "utf-8")
+    fs.writeFileSync(file, scheldwoordenJSON, "utf-8")
     
 
     return channel.send(`je mag niet meer schelden met ${word}`)
