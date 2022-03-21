a3891bd0cd89bb560bc2b400c03923170dc6ec89
TilburgSurviance <101642623+TilburgSurviance@users.noreply.github.com>
2022-03-21 10:00:49 +0100

    a

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ function RandomXP(message) {
 
     var randomXP = Math.floor(Math.random() * 15) + 1
 
-    console.log(randomXP)
+    console.log(`XP: ${randomXP} Member: ${message.author.tag}`)
 }
 client.login(process.env.token)
 
