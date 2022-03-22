269a77a8163694e275ebcd27e0dcbd88a5a0a1fe
TilburgSurviance <101642623+TilburgSurviance@users.noreply.github.com>
2022-03-22 19:19:11 +0100

    help

--- commands/help.js
+++ commands/help.js
@@ -1,12 +1,52 @@
 const discord = require("discord.js")
 const config  = require("./config/cmdconfig.json")
 const index = require("../index.js")
-module.exports.run  = async (bot, message, args) => {
+module.exports.run  = async (client, message, args) => {
 
 
     try {
 
         var prefix = "="
+
+        var response = "**Bot Commands**\r\n\n"
+
+    var general = "**__Algemeen__**\r\n"
+    var Informatie = "**Informatie**\r\n"
+    var Moderatie = "**__Moderatie__**\r\n"
+
+client.commands.forEach(command => {
+    
+
+    switch (command.help.catogory) {
+        case "general" += `${prefix}${command.help.name} - ${command.help.description}`:
+            general += `${prefix}${command.help.name} - ${command.help.description}`
+            break;
+
+            case "Info":
+                Informatie += `${prefix}${command.help.name} - ${command.help.description}`
+            break
+    
+            case "Moderatie" : 
+            Moderatie += `${prefix}${command.help.name} - ${command.help.description}`
+            break
+        
+
+    }
+
+
+});
+
+response += general + Informatie + Moderatie
+
+message.author.send(response).then(() => {
+    return message.channel.send("help staat in je dm")
+    message.author.send("Jehtoch")
+}).catch(() => {
+    return message.channel.send("Je prive berichten staan uit")
+})
+
+
+
 
         
     } catch (error) {
