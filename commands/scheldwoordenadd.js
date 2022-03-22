2fac1d6eee210a1be43bb6fbdb0b9c7c1886e2cb
TilburgSurviance <101642623+TilburgSurviance@users.noreply.github.com>
2022-03-22 07:58:41 +0100

    a

--- commands/scheldwoordenadd.js
+++ commands/scheldwoordenadd.js
@@ -3,30 +3,24 @@ const fs = require("fs")
 const Perms = require("permissions")
 const file = require("../data/woorden.json")
 module.exports.run  = async (client, message, args) => {
-    const member = message.member
-    const channel = message.channel
-
-    if (!member.permissions.has("KICK_MEMBERS")) return message.reply("jij kan dit niet doen. ");
-
-    if(args[1] ) return channel.send("Geef een vloekwoord op")
-
-    var word = args[1]
-
-    var swearwordsJson =  fs.readFileSync("../data/woorden.json", "utf-8");
-    var scheldwoorden = JSON.parse(scheldwoordenJSON)
-
-    scheldwoorden.push(word)
-
-scheldwoordenJSON = JSON.stringify(scheldwoorden)
-    fs.writeFileSync("../data/woorden.json", scheldwoordenJSON, "utf8")
-    
-
-    return channel.send(`je mag niet meer schelden met ${word}`)
+ 
+    if (!message.member.permissions.has("KICK_MEPBERS")) return message.reply("jij kan dit niet doen.");
 
 
-    if(args[1] === "hoofd") {
-        channel.send("hoofd schouders etc..")
-    }
+if(args[0]) return message.reply("Geen vloekwoord mee gegeven")
+
+var word = args[0].tolowerCase();
+
+var jsonfile = fs.readFileSync("../data/woorden.json", "utf-8")
+var woorden = JSON.parse(jsonfile)
+
+woorden.push(word)
+
+jsonfile   = JSON.stringify(woorden)
+fs.writeFileSync("../data/woorden.json", jsonfile, "utf8")
+
+
+return message.channel.send(`Je mag niet meer vloeken/schelden met ${word}`)
 }
 
 module.exports.help = {
